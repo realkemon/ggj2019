d27ff608ba3d907045d68d81bb13a77bf35b20c0
Stefan Ensmann <xoomer@web.de>
2019-01-26 16:16:27 +0100

    added obstacles to level 1

---
+++ Assets/Scripts/Obstacle.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnDrawGizmos() {
+        Gizmos.color = Color.red;
+        Gizmos.DrawSphere(transform.position, 0.1f);
+    }
+}
