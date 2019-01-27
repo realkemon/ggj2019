f46d3f5713489d40915f77c490bc5ed1a0d5983c
Cedric <et.kemon@hotmail.de>
2019-01-27 16:10:01 +0100

    lvl6

---
+++ Assets/Scripts/clock_handle.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class clock_handle : MonoBehaviour
+{
+    public float speed1 = 10.0f; // speed (6.28 means about 1 second)
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, 0, speed1 * Time.deltaTime);
+    }
+}
