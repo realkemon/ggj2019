c17f4a8276c31966a9fd3d29657424df1bdebef5
Cedric <et.kemon@hotmail.de>
2019-01-27 12:37:11 +0100

    mainmenu music

---
+++ Assets/Scripts/Music.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Music : MonoBehaviour
+{
+    AudioSource fxSound; // Emitir sons
+    public AudioClip backMusic;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    void Start()
+    {
+        // Audio Source responsavel por emitir os sons
+        fxSound = GetComponent<AudioSource>();
+        fxSound.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
