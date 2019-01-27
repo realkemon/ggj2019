b5042282d617985dff02a37891d0b521d72d119f
Cedric <et.kemon@hotmail.de>
2019-01-27 01:45:50 +0100

    platforms

---
+++ Assets/Scripts/platform.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class platform : MonoBehaviour
+{
+    public Vector3 position1;
+    public Vector3 position2;
+    public Vector3 targetPosition;
+
+    private bool goingDown;
+    private float moveDistance = 5f;
+    private float lerpTime = 5;
+    private float currentLerpTime = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = position1;
+        goingDown = true;
+        currentLerpTime = 0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*
+        // UP
+        if (transform.position == position1)
+        {
+            goingDown = true;
+            currentLerpTime = 0;
+            targetPosition = position2;
+        }
+
+        // DOWN
+        if (transform.position == position2)
+        {
+            goingDown = false;
+            currentLerpTime = 0;
+            targetPosition = position1;
+        }
+        */
+
+        if (goingDown == true)
+        {
+            currentLerpTime += Time.deltaTime;
+            if (currentLerpTime >= lerpTime)
+            {
+                currentLerpTime = lerpTime;
+                goingDown = false;
+            }
+        }
+        else
+        {
+            currentLerpTime -= Time.deltaTime;
+            if (currentLerpTime <= 0f)
+            {
+                currentLerpTime = 0f;
+                goingDown = true;
+            }
+        }
+        transform.position = Vector3.Lerp(position1, position2, currentLerpTime / lerpTime);
+    }
+
+    void OnValidate()
+    {
+        transform.position = position1;
+    }
+}
